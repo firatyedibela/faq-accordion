c1c9ca16d7ce3cb28e7f4b47b74502209e78ab46
firatyedibela <yedibelafirat0@gmail.com>
2025-06-23 17:34:35 +0300

    feat: implement AccordionItem component

---
+++ src/components/AccordionItem.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import minus from '../assets/icon-minus.svg';
+import plus from '../assets/icon-plus.svg';
+
+type AccordionItemProps = {
+  title: string;
+  answer: string;
+};
+
+export function AccordionItem({ title, answer }: AccordionItemProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleTitleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="accordion-item flex flex-col gap-6">
+      <button
+        onClick={handleTitleClick}
+        className="w-full text-start flex items-center justify-between"
+      >
+        <p className="text-start text-dark-purple  max-w-[225px] leading-[19px] font-semibold">
+          {title}
+        </p>
+        <img
+          src={isOpen ? minus : plus}
+          alt={`${isOpen ? 'Minus' : 'Plus'} icon`}
+        />
+      </button>
+      {isOpen && (
+        <div className="answer text-sm text-light-purple leading-[150%]">
+          {answer}
+        </div>
+      )}
+    </div>
+  );
+}
