bf834921b1b10ad9c01843a97f16cc639750e392
firatyedibela <yedibelafirat0@gmail.com>
2025-06-23 16:15:33 +0300

    feat: implement accordion header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import star from './assets/icon-star.svg';
+
 function App() {
-  return <h1 className="text-center font-bold">Hello World</h1>;
+  return (
+    <main className="p-6 min-h-screen flex items-center justify-center">
+      <div className="accordion max-w-[600px] bg-white rounded-lg p-6 md:p-[40px]">
+        <div className="accordion-header flex gap-6 items-center font-main leading-[38px] md:leading-[65px]">
+          <img
+            className="w-[21px] h-[21px] md:w-[40px] md:h-[40px]"
+            src={star}
+            alt="Star icon"
+          />
+          <h1 className="text-dark-purple font-bold text-[32px] md:text-[56px]  ">
+            FAQs
+          </h1>
+        </div>
+        <div className="accordion-body"></div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
